3c7fcbc9f238f620357fabb5c7850e5f6065cc5b
Vinícius Massari <stonefields11@hotmail.com>
2023-01-27 20:05:07 -0300

    keyboard support

--- src/index.js
+++ src/index.js
@@ -2,35 +2,30 @@ import "./sass/styles.scss"
 
 document.documentElement.classList.add("js")
 
-const accordionOpener = document.querySelectorAll("dt img")
-const accordionQuestion = document.querySelectorAll("dt")
-const accordion = document.querySelectorAll("dd")
+const accordionOpener = document.querySelectorAll("button")
 const events = ["click", "touchstart"]
 
-if (document.documentElement.classList.contains("js") && accordionOpener && accordionQuestion) {
-    
 
-    function openAccordion(event) {
-        event.preventDefault()
-       
-        accordionQuestion.forEach(question =>{
-            question.classList.remove("selected")
-        })
-        accordion.forEach(accordion =>{
-            accordion.classList.remove("selected")
-        })
-        event.target.parentElement.classList.toggle("selected")
-        event.target.parentElement.nextElementSibling.classList.toggle("selected");
+
+function toggleAccordion({currentTarget}) {
+    const hasAria = currentTarget.getAttribute("aria-expanded")
+    if (hasAria == "false") {
+        currentTarget.children[0].classList.add("selected")
+        currentTarget.parentElement.nextElementSibling.removeAttribute("hidden")
+        currentTarget.parentElement.nextElementSibling.children[0].classList.add("selected")
+        currentTarget.setAttribute("aria-expanded", "true")
+    } else{
+        currentTarget.parentElement.nextElementSibling.setAttribute("hidden", true)
+        currentTarget.setAttribute("aria-expanded", "false")
+        currentTarget.children[0].classList.remove("selected")
+        currentTarget.parentElement.nextElementSibling.children[0].classList.remove("selected")
     }
+ 
+   
+}
 
 events.forEach(userEvent =>{
     accordionOpener.forEach(opener =>{
-        opener.addEventListener(userEvent, openAccordion)
+        opener.addEventListener(userEvent, toggleAccordion)
     })
 })
-
-
-
-
-
-}
